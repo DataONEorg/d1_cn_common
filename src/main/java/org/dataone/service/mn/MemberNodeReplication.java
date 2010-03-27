92fd191a1995eeda04ea40ebf2f360d989390dd2
Matt Jones <jones@nceas.ucsb.edu>
2010-03-27 00:58:41 +0000

    Remove the synchronizationComplete() and synchronizationFailed() methods as they are no longer in the spec.

--- src/main/java/org/dataone/service/mn/MemberNodeReplication.java
+++ src/main/java/org/dataone/service/mn/MemberNodeReplication.java
@@ -2,9 +2,7 @@ package org.dataone.service.mn;
 
 import org.dataone.service.exceptions.InvalidRequest;
 import org.dataone.service.exceptions.NotAuthorized;
-import org.dataone.service.exceptions.NotFound;
 import org.dataone.service.types.AuthToken;
-import org.dataone.service.types.IdentifierType;
 
 /**
  * The DataONE MemberNode Replication programmatic interface.  This defines an
@@ -17,11 +15,4 @@ public interface MemberNodeReplication
 {
     public void listObjects(AuthToken token, String query) 
         throws NotAuthorized, InvalidRequest;
-    
-    // Didn't we eliminate these methods at the last CCIT meeting as not being
-    // needed if MNs are not aksed to track replicated objects?
-    public void synchronizationComplete(AuthToken token, IdentifierType guid)
-        throws NotAuthorized, NotFound;
-    public void synchronizationFailed(AuthToken token, IdentifierType guid, 
-            String problemDescription) throws NotAuthorized, NotFound;
 }
