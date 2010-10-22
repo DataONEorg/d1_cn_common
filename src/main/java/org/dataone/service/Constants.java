9f18195e14bd22825ed3b72c9a0faca846ff9c37
Chad Berkley <berkley@nceas.ucsb.edu>
2010-10-22 20:17:53 +0000

    adding client side constants to the service api.  this used to be in d1-java but it can now be shared across clients

---
+++ src/main/java/org/dataone/service/Constants.java
@@ -0,0 +1,52 @@
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright ${year}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id$
+ */
+package org.dataone.service;
+
+/**
+ * @author berkley
+ * A class to contain constants used in clients 
+ */
+public class Constants
+{
+    /** HTTP Verb GET */
+    public static final String GET = "GET";
+    /** HTTP Verb POST */
+    public static final String POST = "POST";
+    /** HTTP Verb PUT */
+    public static final String PUT = "PUT";
+    /** HTTP Verb DELETE */
+    public static final String DELETE = "DELETE";    
+    /** API OBJECTS Resource which handles with document operations */
+    public static final String RESOURCE_OBJECTS = "object";
+    /** API META Resource which handles SystemMetadata operations */
+    public static final String RESOURCE_META = "meta";
+    /** API SESSION Resource which handles with user session operations */
+    public static final String RESOURCE_SESSION = "session";
+    /** API RESOLVE Resource which handles resolve operations */
+    public static final String RESOURCE_RESOLVE = "resolve";
+    /** API IDENTIFIER Resource which controls object identifier operations */
+    public static final String RESOURCE_IDENTIFIER = "identifier";
+    /** API LOG controls logging events */
+    public static final String RESOURCE_LOG = "log";
+    /** The URL string for the node REST API */
+    private String nodeBaseServiceUrl;
+}
