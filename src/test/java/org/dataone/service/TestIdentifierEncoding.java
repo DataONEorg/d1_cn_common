8d93a43fb6a51e147c647b9657b72dced72aa655
Rob Nahf <rnahf@epscor.unm.edu>
2010-11-18 23:13:47 +0000

    Refactored the TestIdentifierEncoding to move the test cases (character strings and their escaped counterparts) into a standalone utf8 file that can be shared and maintained separately.

--- src/test/java/org/dataone/service/TestIdentifierEncoding.java
+++ src/test/java/org/dataone/service/TestIdentifierEncoding.java
@@ -2,143 +2,110 @@ package org.dataone.service;
 
 import static org.junit.Assert.*;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-// import org.apache.commons.codec.binary.Hex;
 import org.dataone.service.EncodingUtilities;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestIdentifierEncoding 
 {
-
-	// finding the proper escaped string was the challenge here, as was finding a way to put supplementary
-	// unicode characters into Java Strings
-	// see http://www.utf8-chartable.de/unicode-utf8-table.pl for a thorough Unicode to UTF-8 reference
-	// the O'Reilly Java in a Nutshell book (5th edition) pg. 209-210 provided the way to put supplementary
-	// code points into Strings.
-
+	private static String testUnicodeIdentifiersFile = "/org/dataone/service/encodingTestSet/testUnicodeStrings.utf8.txt";
+	private static HashMap<String,String> StandardTests = new HashMap<String,String>();
 	
-	private static HashMap<String, String> commonEncodingPairs = new HashMap<String, String>();
-	static {
-		// exhaustive list of allowable characters
-		commonEncodingPairs.put("allowed-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-				                "allowed-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-		commonEncodingPairs.put("allowed-0123456789","allowed-0123456789");
-		commonEncodingPairs.put("allowed-;:@$-_.!*()',~", "allowed-;:@$-_.!*()',~");
-		
-		commonEncodingPairs.put("escaping-anyway:+", "escaping-anyway:%2B");
-
-		commonEncodingPairs.put("disallowed-ascii-spaces: x x ", "disallowed-ascii-spaces:%20x%20x%20");
-		commonEncodingPairs.put("disallowed-ascii-percent:%", "disallowed-ascii-percent:%25");
-		commonEncodingPairs.put("disallowed-ascii-others:\"#<>[]\\^`{}|", "disallowed-ascii-others:%22%23%3C%3E%5B%5D%5C%5E%60%7B%7D%7C");
-		commonEncodingPairs.put("disallowed-nonAscii-BMP-umlauted-u:" + "\u00FC", "disallowed-nonAscii-BMP-umlauted-u:%C3%BC");
-		commonEncodingPairs.put("disallowed-nonAscii-BMP-euro:" + "\u20AC", "disallowed-nonAscii-BMP-euro:%E2%82%AC");
-
-		commonEncodingPairs.put("disallowed-nonAscii-BMP-thai:" + "\u0E09", "disallowed-nonAscii-BMP-thai:%E0%B8%89");
-		
-		int codepoint = 0x1010C;  // An Aegean number
-		char[] surrogatePair = Character.toChars(codepoint);
-		String supp = new String(surrogatePair);
-		commonEncodingPairs.put("disallowed-nonAscii-supplementary-AegeanNums:" + supp,"disallowed-nonAscii-supplementary-AegeanNums:%F0%90%84%8C");
-		
-		
-		// can't get control character testing to work, for perhaps predictable reasons		
-//		StringBuilder controlID = new StringBuilder(32);
-//		for (int i = 0x00; i < 0x1F; i++)		controlID.append(Character.toChars(i));
-//		String encodedControlID = "%00%01%02%03%04%05%06%07%08%09%0A%0B%0C%0D%0E%0F%10%11%12%13%14%15%16%17%18%19%1A%1B%1C%1D%1E%1F";
-//		commonEncodingPairs.put("disallowed-ascii-controls:" + controlID.toString(),"disallowed-ascii-controls:" + encodedControlID);
+	@Before
+	public void generateStandardTests() {
+		if (StandardTests.size() == 0) {
+			log(" * * * * * * * Unicode Test Strings * * * * * * ");
+			
+			InputStream is = this.getClass().getResourceAsStream(testUnicodeIdentifiersFile);
+			Scanner s = new Scanner(is,"UTF-8");
+			String[] temp;
+			try
+			{
+				while (s.hasNextLine()) 
+				{
+					String line = s.nextLine();
+					log(line);
+					temp = line.split("\t");
+					if (temp.length > 1)
+						StandardTests.put(temp[0], temp[1]);
+				}
+				log("");
+			} finally {
+				s.close();
+			}
+			log("");
+		}
 	}
 	
-	
-	
+
 	@Test
-	public final void testEncodeUrlPathSegment() 
+	public final void testEncodeUrlPathSegment()
 	{
-		System.out.println(" * * * * * * * testing URL Path Segment encoding * * * * * * ");
-		System.out.println();
-		SortedSet<String> ids = new TreeSet<String>(commonEncodingPairs.keySet());
+		log(" * * * * * * * testing URL Path Segment encoding * * * * * * ");
+
+		SortedSet<String> ids = new TreeSet<String>(StandardTests.keySet());
 		Iterator<String> i = ids.iterator();
 		while (i.hasNext())
 		{
 			String id = (String) i.next();
-			runAssertion(id,
-					 commonEncodingPairs.get(id),
-					 EncodingUtilities.encodeUrlPathSegment(id)
-					 );
+			if (id.startsWith("common-") || 
+					id.startsWith("path-")) {
+				runAssertion(id,
+							StandardTests.get(id),
+							EncodingUtilities.encodeUrlPathSegment(id)
+							);
+			}
 		}
-		
-		// these are allowed in paths and fragments, but not queries
-		runAssertion("disallowed-ascii-?/",
-					 "disallowed-ascii-%3F%2F",
-					 EncodingUtilities.encodeUrlPathSegment("disallowed-ascii-?/"));
-		
-		// these are allowed in paths and fragments, but not queries
-		runAssertion("allowed-ascii-&=&=",
-					 "allowed-ascii-&=&=",
-					 EncodingUtilities.encodeUrlPathSegment("allowed-ascii-&=&="));
+		log("");
 	}
 
-
+	
 	@Test
-	public final void testEncodeUrlFragment() 
+	public final void testEncodeUrlQuerySegment()
 	{
-		System.out.println(" * * * * * * * testing URL Fragment encoding * * * * * * ");
-		System.out.println();
-		SortedSet<String> ids = new TreeSet<String>(commonEncodingPairs.keySet());
+		log(" * * * * * * * testing URL Query Segment encoding * * * * * * ");
+		SortedSet<String> ids = new TreeSet<String>(StandardTests.keySet());
 		Iterator<String> i = ids.iterator();
 		while (i.hasNext())
 		{
 			String id = (String) i.next();
-			runAssertion(id,
-						 commonEncodingPairs.get(id),
-						 EncodingUtilities.encodeUrlFragment(id)
-						 );
+			if (id.startsWith("common-") || 
+					id.startsWith("query-")) {
+				runAssertion(id,
+							StandardTests.get(id),
+							EncodingUtilities.encodeUrlQuerySegment(id)
+							);
+			}
 		}
-		// these are allowed in paths and fragments, but not queries
-		runAssertion("allowed-ascii-&=&=",
-					 "allowed-ascii-&=&=",
-					 EncodingUtilities.encodeUrlFragment("allowed-ascii-&=&="));
-
-		
-		// fragments and queries are allowed 2 extra characters to be in the unescaped set: "/" and "?"
-		runAssertion("allowed-ascii-/?/?",
-					 "allowed-ascii-/?/?",
-					 EncodingUtilities.encodeUrlFragment("allowed-ascii-/?/?"));
+		log("");
 	}
 
+	
 	@Test
-	public final void testEncodeUrlQuerySegment() 
+	public final void testEncodeUrlFragment()
 	{
-		System.out.println(" * * * * * * * testing URL Query Segment encoding * * * * * * ");
-		System.out.println();
-		SortedSet<String> ids = new TreeSet<String>(commonEncodingPairs.keySet());
+		log(" * * * * * * * testing URL Fragment encoding * * * * * * ");
+		SortedSet<String> ids = new TreeSet<String>(StandardTests.keySet());
 		Iterator<String> i = ids.iterator();
 		while (i.hasNext())
 		{
 			String id = (String) i.next();
-			runAssertion(id,
-						 commonEncodingPairs.get(id),
-						 EncodingUtilities.encodeUrlQuerySegment(id)
-						 );
+			if (id.startsWith("common-") || 
+					id.startsWith("fragment-")) {
+				runAssertion(id,
+							StandardTests.get(id),
+							EncodingUtilities.encodeUrlFragment(id)
+							);
+			}
 		}
-		// fragments and queries are allowed 2 extra characters to be in the unescaped set: "/" and "?"
-		runAssertion("allowed-ascii-/?/?",
-					 "allowed-ascii-/?/?",
-					 EncodingUtilities.encodeUrlQuerySegment("allowed-ascii-/?/?"));
-
-		
-		// queries need to escape 2 characters that are reserved by convention for separating segments
-		//  the characters "&" and "="
-		runAssertion("escaped-&=&=",
-					 "escaped-%26%3D%26%3D",
-					 EncodingUtilities.encodeUrlQuerySegment("escaped-&=&="));
-		// a meaningful case...
-		runAssertion("escaped-this&that=theOther",
-				 "escaped-this%26that%3DtheOther",
-				 EncodingUtilities.encodeUrlQuerySegment("escaped-this&that=theOther"));
+		log("");
 	}
 
 	
@@ -149,10 +116,11 @@ public class TestIdentifierEncoding
 		System.out.println("    expect:    " + expected);
 		System.out.println("       got:    " + got);
 		System.out.println();
-//		System.out.flush();
+
 		assertTrue("identifier: " + id, got.equals(expected));		
 	}
-	
-	
 
+	  private static void log(Object aObject){
+		  System.out.println(String.valueOf(aObject));
+	  }	  
 }
