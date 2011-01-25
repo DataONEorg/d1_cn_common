dc4c646ca353b05b30b536b8cfde8b4ab21db150
Robert Waltz <rwaltz@utk.edu>
2011-01-25 18:17:53 +0000

    #1257: create server-side implementation in d1_common_java

---
+++ src/main/java/org/dataone/mimemultipart/MultipartRequest.java
@@ -0,0 +1,328 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dataone.mimemultipart;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *  Wrap a HttpServletRequest and add two Map attributes
+ *  to save Form Multipart files and Parameters.
+ *  Added two public methods to access the multipart Files and Parameters.
+ *
+ * @author waltz
+ */
+public class MultipartRequest implements HttpServletRequest {
+
+    private final Map<String, File> multipartFiles;
+    private HttpServletRequest request;
+    private final Map<String, List<String>> multipartParameters;
+
+    /**
+     * Create a new MultipartParsingResult.
+     * @param mpFiles Map of field name to File instance
+     * @param mpParams Map of field name to form field String value
+     */
+    public MultipartRequest(HttpServletRequest request, Map<String, File> mpFiles, Map<String, List<String>> mpParams) {
+        this.multipartFiles = mpFiles;
+        this.multipartParameters = mpParams;
+    }
+
+    /**
+     * Return the multipart files as Map of field name to MultipartFile instance.
+     */
+    public Map<String, File> getMultipartFiles() {
+        return this.multipartFiles;
+    }
+
+    /**
+     * Return the multipart parameters as Map of field name to form field String value.
+     */
+    public Map<String, List<String>> getMultipartParameters() {
+        return this.multipartParameters;
+    }
+
+    @Override
+    public String getAuthType() {
+        return request.getAuthType();
+    }
+
+    @Override
+    public Cookie[] getCookies() {
+        return request.getCookies();
+    }
+
+    @Override
+    public long getDateHeader(String string) {
+        return request.getDateHeader(string);
+    }
+
+    @Override
+    public String getHeader(String string) {
+        return request.getHeader(string);
+    }
+
+    @Override
+    public Enumeration getHeaders(String string) {
+        return request.getHeaders(string);
+    }
+
+    @Override
+    public Enumeration getHeaderNames() {
+        return request.getHeaderNames();
+    }
+
+    @Override
+    public int getIntHeader(String string) {
+        return request.getIntHeader(string);
+    }
+
+    @Override
+    public String getMethod() {
+        return request.getMethod();
+    }
+
+    @Override
+    public String getPathInfo() {
+        return request.getPathInfo();
+    }
+
+    @Override
+    public String getPathTranslated() {
+        return request.getPathTranslated();
+    }
+
+    @Override
+    public String getContextPath() {
+        return request.getContextPath();
+    }
+
+    @Override
+    public String getQueryString() {
+        return request.getQueryString();
+    }
+
+    @Override
+    public String getRemoteUser() {
+        return request.getRemoteUser();
+    }
+
+    @Override
+    public boolean isUserInRole(String string) {
+        return request.isUserInRole(string);
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return request.getUserPrincipal();
+    }
+
+    @Override
+    public String getRequestedSessionId() {
+        return request.getRequestedSessionId();
+    }
+
+    @Override
+    public String getRequestURI() {
+        return request.getRequestURI();
+    }
+
+    @Override
+    public StringBuffer getRequestURL() {
+        return request.getRequestURL();
+    }
+
+    @Override
+    public String getServletPath() {
+        return request.getServletPath();
+    }
+
+    @Override
+    public HttpSession getSession(boolean bln) {
+        return request.getSession(bln);
+    }
+
+    @Override
+    public HttpSession getSession() {
+        return request.getSession();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdValid() {
+        return request.isRequestedSessionIdValid();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromCookie() {
+        return request.isRequestedSessionIdFromCookie();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromURL() {
+        return request.isRequestedSessionIdFromURL();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromUrl() {
+        return request.isRequestedSessionIdFromUrl();
+    }
+
+    @Override
+    public Object getAttribute(String string) {
+        return request.getAttribute(string);
+    }
+
+    @Override
+    public Enumeration getAttributeNames() {
+        return request.getAttributeNames();
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return request.getCharacterEncoding();
+    }
+
+    @Override
+    public void setCharacterEncoding(String string) throws UnsupportedEncodingException {
+        request.setCharacterEncoding(string);
+    }
+
+    @Override
+    public int getContentLength() {
+        return request.getContentLength();
+    }
+
+    @Override
+    public String getContentType() {
+        return request.getContentType();
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return request.getInputStream();
+    }
+
+    @Override
+    public String getParameter(String string) {
+        return request.getParameter(string);
+    }
+
+    @Override
+    public Enumeration getParameterNames() {
+        return request.getParameterNames();
+    }
+
+    @Override
+    public String[] getParameterValues(String string) {
+        return request.getParameterValues(string);
+    }
+
+    @Override
+    public Map getParameterMap() {
+        return request.getParameterMap();
+    }
+
+    @Override
+    public String getProtocol() {
+        return request.getProtocol();
+    }
+
+    @Override
+    public String getScheme() {
+        return request.getScheme();
+    }
+
+    @Override
+    public String getServerName() {
+        return request.getServerName();
+    }
+
+    @Override
+    public int getServerPort() {
+        return request.getServerPort();
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return request.getReader();
+    }
+
+    @Override
+    public String getRemoteAddr() {
+        return request.getRemoteAddr();
+    }
+
+    @Override
+    public String getRemoteHost() {
+        return request.getRemoteHost();
+    }
+
+    @Override
+    public void setAttribute(String string, Object o) {
+        request.setAttribute(string, o);
+    }
+
+    @Override
+    public void removeAttribute(String string) {
+        request.removeAttribute(string);
+    }
+
+    @Override
+    public Locale getLocale() {
+        return request.getLocale();
+    }
+
+    @Override
+    public Enumeration getLocales() {
+        return request.getLocales();
+    }
+
+    @Override
+    public boolean isSecure() {
+        return request.isSecure();
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String string) {
+        return request.getRequestDispatcher(string);
+    }
+
+    @Override
+    public String getRealPath(String string) {
+        return request.getRealPath(string);
+    }
+
+    @Override
+    public int getRemotePort() {
+        return request.getRemotePort();
+    }
+
+    @Override
+    public String getLocalName() {
+        return request.getLocalName();
+    }
+
+    @Override
+    public String getLocalAddr() {
+        return request.getLocalAddr();
+    }
+
+    @Override
+    public int getLocalPort() {
+        return request.getLocalPort();
+    }
+}
---
+++ src/main/java/org/dataone/mimemultipart/MultipartRequestResolver.java
@@ -0,0 +1,175 @@
+/**
+ * 
+ */
+package org.dataone.mimemultipart;
+
+import java.io.*;
+import java.util.*;
+
+
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * DataOne conversion of HttpServletRequests into a DataONE MultipartRequest via
+ *  <a href="http://jakarta.apache.org/commons/fileupload">Jakarta Commons FileUpload</a>
+ * 1.2 or above. Inspired by <a href="http://static.springsource.org/spring/docs/3.0.x/javadoc-api/org/springframework/web/multipart/MultipartResolver.html">MultipartResolver</a>
+ * but almost entirely rewritten by DataONE.
+ *
+ * <p>Provides "maxUploadSize", "maxMemorySize" and "tmpUploadDir" settings as
+ * bean properties. See corresponding
+ * ServletFileUpload / DiskFileItemFactory properties ("sizeMax", "sizeThreshold",
+ * ) for details in terms of defaults and accepted values.
+ *
+ * <p>Saves temporary files to the temporary directory.
+ *
+ * @author Robert P. Waltz (DataONE)
+ * @author Trevor D. Cook (Original)
+ * @author Juergen Hoeller (Original)
+ * @since 29.09.2003
+ * @see #CommonsMultipartResolver(ServletContext)
+ * @see org.springframework.web.portlet.multipart.CommonsPortletMultipartResolver
+ * @see org.apache.commons.fileupload.servlet.ServletFileUpload
+ * @see org.apache.commons.fileupload.disk.DiskFileItemFactory
+ */
+public class MultipartRequestResolver
+{
+
+    private DiskFileItemFactory factory;
+    private ServletFileUpload upload;
+    /**
+     * constructor.  
+     * @param filePartFile a Directory path to write any file contents to.
+     *
+     */
+    public MultipartRequestResolver()
+    {
+        // Create a factory for disk-based file items
+        this.factory = new DiskFileItemFactory();
+
+        // Create a new file upload handler
+        this.upload = new ServletFileUpload(this.factory);
+
+    }
+    public MultipartRequestResolver(String tmpUploadDir)
+    {
+        this.factory = new DiskFileItemFactory();
+        this.factory.setRepository(new File(tmpUploadDir));
+        this.upload = new ServletFileUpload(factory);
+    }
+    public MultipartRequestResolver(String tmpUploadDir, int maxUploadSize)
+    {
+        this.factory = new DiskFileItemFactory();
+
+        // Set factory constraints
+        this.factory.setRepository(new File(tmpUploadDir));
+
+        // Create a new file upload handler
+        this.upload = new ServletFileUpload(factory);
+
+        // Set overall request size constraint
+        this.upload.setSizeMax(maxUploadSize);
+
+    }
+    public MultipartRequestResolver(String tmpUploadDir, int maxUploadSize, int maxMemorySize)
+    {
+        this.factory = new DiskFileItemFactory();
+
+        // Set factory constraints
+        this.factory.setSizeThreshold(maxMemorySize);
+        this.factory.setRepository(new File(tmpUploadDir));
+
+        // Create a new file upload handler
+        this.upload = new ServletFileUpload(factory);
+
+        // Set overall request size constraint
+        this.upload.setSizeMax(maxUploadSize);
+
+    }
+    /**
+     * get the parts of a
+     *
+     * @throws IOException 
+     */
+    public MultipartRequest resolveMultipart(HttpServletRequest request) throws IOException, FileUploadException
+    {
+        Map<String, List<String>> mpParams = new HashMap<String, List<String>>();
+        Map<String, File> mpFiles = new HashMap<String, File>();
+        MultipartRequest multipartRequest = new MultipartRequest(request, mpFiles, mpParams);
+        if (!this.upload.isMultipartContent(request)) {
+            return multipartRequest;
+        }
+        List /* FileItem */ items = upload.parseRequest(request);
+        Iterator iter = items.iterator();
+        while (iter.hasNext()) {
+            FileItem item = (FileItem) iter.next();
+
+            if (item.isFormField()) {
+                String name = item.getFieldName();
+                String value = item.getString();
+                if (mpParams.containsKey(name)) {
+                    mpParams.get(name).add(value);
+                } else {
+                    List values = new ArrayList();
+                    values.add(value);
+                    mpParams.put(name, values);
+                }
+            } else {
+               // processUploadedFile(item);
+                File itemFile = new File(item.getName());
+                String fileKey = getOriginalFilename(item);
+                mpFiles.put(fileKey, itemFile);
+            }
+        }
+        return multipartRequest;
+    }
+ /**
+ * From the MultipartFile Class implementation for Jakarta Commons FileUpload.
+ *<a href="http://static.springsource.org/spring/docs/3.0.x/javadoc-api/org/springframework/web/multipart/commons/CommonsMultipartFile.html"> CommonsMultipartFile </a>
+ *
+ * @author Trevor D. Cook
+ * @author Juergen Hoeller
+ * @since 29.09.2003
+ */
+    private String getOriginalFilename(FileItem fileItem) {
+            String filename = fileItem.getName();
+            if (filename == null) {
+                    // Should never happen.
+                    return "";
+            }
+            // check for Unix-style path
+            int pos = filename.lastIndexOf("/");
+            if (pos == -1) {
+                    // check for Windows-style path
+                    pos = filename.lastIndexOf("\\");
+            }
+            if (pos != -1)  {
+                    // any sort of path separator found
+                    return filename.substring(pos + 1);
+            }
+            else {
+                    // plain name
+                    return filename;
+            }
+    }
+}
