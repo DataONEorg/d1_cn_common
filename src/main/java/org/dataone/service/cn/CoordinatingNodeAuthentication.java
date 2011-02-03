d1623042e8a7b92d572920556db4f9eb3040630d
Robert Waltz <rwaltz@utk.edu>
2011-02-03 03:42:38 +0000

    updated CoordinatingNode Interfaces

---
+++ src/main/java/org/dataone/service/cn/CoordinatingNodeAuthentication.java
@@ -0,0 +1,82 @@
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright ${year}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataone.service.cn;
+
+import java.security.cert.X509Extension;
+import java.util.List;
+import org.dataone.service.exceptions.AuthenticationTimeout;
+import org.dataone.service.exceptions.IdentifierNotUnique;
+import org.dataone.service.exceptions.InvalidCredentials;
+import org.dataone.service.exceptions.InvalidRequest;
+import org.dataone.service.exceptions.InvalidToken;
+import org.dataone.service.exceptions.NotAuthorized;
+import org.dataone.service.exceptions.NotFound;
+import org.dataone.service.exceptions.NotImplemented;
+import org.dataone.service.exceptions.ServiceFailure;
+import org.dataone.service.types.AccessRule;
+import org.dataone.service.types.AuthToken;
+import org.dataone.service.types.AuthType;
+import org.dataone.service.types.Identifier;
+import org.dataone.service.types.Principal;
+
+/**
+ * The DataONE Coordinating Node Authorization programmatic interface.  This defines an
+ * implementation interface for Coordinating Nodes that wish to build an
+ * implementation that is compliant with the DataONE service definitions.
+ *
+ * @author Matthew Jones
+ */
+public interface CoordinatingNodeAuthentication
+{
+    public AuthToken login(String user, String password)
+        throws InvalidCredentials, AuthenticationTimeout, ServiceFailure, NotImplemented, InvalidRequest;
+
+    public AuthToken login(String user, String password, AuthType type)
+            throws InvalidCredentials, AuthenticationTimeout, NotImplemented, InvalidRequest, ServiceFailure;
+
+    public AuthToken getAuthToken(X509Extension cert)
+            throws InvalidCredentials, AuthenticationTimeout, ServiceFailure, NotImplemented, InvalidRequest ;
+    //public void logout(AuthToken token) 
+    //    throws InvalidCredentials, AuthenticationTimeout;
+  
+    public Identifier setOwner(AuthToken token, Identifier pid, Principal userId)
+            throws ServiceFailure, InvalidToken, NotAuthorized, NotFound, NotImplemented, InvalidRequest;
+
+     public Principal newAccount(String username, String password)
+            throws ServiceFailure, IdentifierNotUnique, InvalidCredentials, NotImplemented, InvalidRequest;
+
+     public Principal newAccount(String username, String password, AuthType type)
+            throws ServiceFailure, IdentifierNotUnique, InvalidCredentials, NotImplemented, InvalidRequest;
+
+     public boolean verifyToken(AuthToken token)
+             throws ServiceFailure, NotAuthorized, NotImplemented, InvalidToken, InvalidRequest;
+
+     public boolean mapIdentity(AuthToken token1, AuthToken token2)
+             throws ServiceFailure, InvalidToken, NotAuthorized, NotFound, NotImplemented, InvalidRequest;
+
+     public Identifier createGroup(AuthToken token, Identifier groupName)
+             throws ServiceFailure, InvalidToken, NotAuthorized, NotFound, NotImplemented, InvalidRequest, IdentifierNotUnique;
+
+     public Identifier createGroup(AuthToken token, Identifier groupName, List<Identifier> members)
+             throws ServiceFailure, InvalidToken, NotAuthorized, NotFound, NotImplemented, InvalidRequest;
+    
+
+}
--- src/main/java/org/dataone/service/cn/CoordinatingNodeAuthorization.java
+++ src/main/java/org/dataone/service/cn/CoordinatingNodeAuthorization.java
@@ -1,64 +1,36 @@
-/**
- * This work was created by participants in the DataONE project, and is
- * jointly copyrighted by participating institutions in DataONE. For
- * more information on DataONE, see our web site at http://dataone.org.
- *
- *   Copyright ${year}
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 package org.dataone.service.cn;
 
-import org.dataone.service.exceptions.AuthenticationTimeout;
-import org.dataone.service.exceptions.IdentifierNotUnique;
-import org.dataone.service.exceptions.InvalidCredentials;
+import org.dataone.service.exceptions.InvalidRequest;
 import org.dataone.service.exceptions.InvalidToken;
 import org.dataone.service.exceptions.NotAuthorized;
 import org.dataone.service.exceptions.NotFound;
+import org.dataone.service.exceptions.NotImplemented;
 import org.dataone.service.exceptions.ServiceFailure;
-import org.dataone.service.types.AccessRule;
+import org.dataone.service.types.AccessPolicy;
 import org.dataone.service.types.AuthToken;
+import org.dataone.service.types.Event;
 import org.dataone.service.types.Identifier;
-import org.dataone.service.types.Principal;
 
 /**
- * The DataONE Coordinating Node Authorization programmatic interface.  This defines an
- * implementation interface for Coordinating Nodes that wish to build an
- * implementation that is compliant with the DataONE service definitions.
  *
- * @author Matthew Jones
+ * @author waltz
  */
-public interface CoordinatingNodeAuthorization 
-{
-    public AuthToken login(String user, String password)
-        throws InvalidCredentials, AuthenticationTimeout, ServiceFailure;
-    
-    //public void logout(AuthToken token) 
-    //    throws InvalidCredentials, AuthenticationTimeout;
-    
-    public boolean setAccess(AuthToken token, Identifier id, String principal,
-            String permission, String permissionType, String permissionOrder)
-            throws ServiceFailure;
-    
-    public Identifier setOwner(AuthToken token, Identifier guid, Principal userId)
-        throws InvalidToken, NotAuthorized, NotFound;
-    
-    public Principal newAccount(String username, String password)
-        throws IdentifierNotUnique, InvalidCredentials;
-    
-    public boolean verify(AuthToken token) throws NotAuthorized;
-    
-    public boolean isAuthorized(AuthToken token, Identifier guid, String operation)
-        throws InvalidToken, NotFound, NotAuthorized;
+public interface CoordinatingNodeAuthorization {
+
+
+    public boolean isAuthorized(AuthToken token, Identifier pid, Event operation)
+        throws ServiceFailure, InvalidToken, NotFound, NotAuthorized, NotImplemented, InvalidRequest;
+
+    public boolean setAccess(AuthToken token, Identifier pid, AccessPolicy accessPolicy)
+            throws InvalidToken, ServiceFailure, NotFound, NotAuthorized, NotImplemented, InvalidRequest;
+
+    // this is the signature that setAcess has now, should be deprecated in future
+    public boolean setAccess(AuthToken token, Identifier pid, String principal, String permission,
+            String permissionType, String permissionOrder)
+            throws InvalidToken, ServiceFailure, NotFound, NotAuthorized, NotImplemented, InvalidRequest;
 }
