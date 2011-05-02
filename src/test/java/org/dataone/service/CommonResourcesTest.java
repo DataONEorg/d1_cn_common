1aec3d0dcba6bb27bb7f848eaec8a49d92bb7a45
Rob Nahf <rnahf@epscor.unm.edu>
2011-05-02 22:05:34 +0000

    addition of d1_test_resources package (and a unit test for) to the pom.xml.  (Makes the shared resources available to all packages using d1_common_java without any addtional configuration).

---
+++ src/test/java/org/dataone/service/CommonResourcesTest.java
@@ -0,0 +1,26 @@
+package org.dataone.service;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+/**
+ * Test to make sure this package can access the common resources found in
+ * the d1_test_resources package.
+ * 
+ * @author rnahf
+ */
+public class CommonResourcesTest {
+
+	@Test
+	public final void testCommonResourcesAvailability() throws IOException
+	{
+		String resource = "/D1shared/selfTest/simpleDummyResource.txt";
+		InputStream is = this.getClass().getResourceAsStream(resource);
+		if (is == null)
+			fail("could not find resource: " + resource);
+	}
+}
