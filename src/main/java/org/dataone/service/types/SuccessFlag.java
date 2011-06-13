83e15dcca3a4ec1e9e7e4ac4cf47ef354061ac43
Robert Waltz <rwaltz@utk.edu>
2011-06-13 18:20:03 +0000

    0.6.2-SNAPSHOT changes; changed successFlag

--- src/main/java/org/dataone/service/types/SuccessFlag.java
+++ src/main/java/org/dataone/service/types/SuccessFlag.java
@@ -2,34 +2,38 @@
 package org.dataone.service.types;
 
 /** 
+ * Store last time checked information from the Mn_Health.status method
+ Other information from status will be stored in the Service Node
+ A process should check  MN_health.getStatus()  periodically and  
+ update the version and availability  for each service.
+
+ * 
  * Schema fragment(s) for this class:
  * <pre>
  * &lt;xs:complexType xmlns:ns="http://ns.dataone.org/service/types/0.6.2" xmlns:xs="http://www.w3.org/2001/XMLSchema" name="SuccessFlag">
- *   &lt;xs:simpleContent>
- *     &lt;xs:extension base="xs:boolean"/>
- *   &lt;/xs:simpleContent>
+ *   &lt;xs:attribute type="xs:boolean" name="value"/>
  * &lt;/xs:complexType>
  * </pre>
  */
 public class SuccessFlag
 {
-    private boolean _boolean;
+    private Boolean value;
 
     /** 
-     * Get the 'SuccessFlag' complexType value.
+     * Get the 'value' attribute value.
      * 
      * @return value
      */
-    public boolean isBoolean() {
-        return _boolean;
+    public Boolean getValue() {
+        return value;
     }
 
     /** 
-     * Set the 'SuccessFlag' complexType value.
+     * Set the 'value' attribute value.
      * 
-     * @param _boolean
+     * @param value
      */
-    public void setBoolean(boolean _boolean) {
-        this._boolean = _boolean;
+    public void setValue(Boolean value) {
+        this.value = value;
     }
 }
