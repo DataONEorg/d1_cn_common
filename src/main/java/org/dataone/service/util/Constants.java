4e8788360306bc4362f7ab0e060d6a3f41e513db
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2011-10-25 21:32:59 +0000

    propagate to client and rest service:
    refactor mapIdentity() to be requestMapIdentity().
    old mapIdentity() method now has 2 Subject parameters and a Session (which is an MN/admin user).
    This allows two identities to be mapped in one method call, provided we trust the Session.

--- src/main/java/org/dataone/service/util/Constants.java
+++ src/main/java/org/dataone/service/util/Constants.java
@@ -76,6 +76,8 @@ public class Constants {
     public static final String RESOURCE_ACCOUNTS = "accounts";
     /** IDENTITY API resource which handles account-mapping operations */
     public static final String RESOURCE_ACCOUNT_MAPPING = "accounts/map";
+    /** IDENTITY API resource which handles account-mapping request operations */
+    public static final String RESOURCE_ACCOUNT_MAPPING_REQUEST = "accounts/maprequest";
     /** IDENTITY API resource which handles account-mapping confirmation operations */
     public static final String RESOURCE_ACCOUNT_MAPPING_CONFIRM = "accounts/mapconfirm";
     /** IDENTITY API resource which handles pending account-mapping operations */
