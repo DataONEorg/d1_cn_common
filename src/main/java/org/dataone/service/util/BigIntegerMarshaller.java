986c2aac2ad87b5c4b389bb4b8936dfc68afc227
Robert Waltz <rwaltz@utk.edu>
2012-01-20 18:48:36 +0000

    #1914: Big Integers need to be limited to length of unsigned long

--- src/main/java/org/dataone/service/util/BigIntegerMarshaller.java
+++ src/main/java/org/dataone/service/util/BigIntegerMarshaller.java
@@ -15,7 +15,8 @@ import org.jibx.runtime.JiBXException;
  */
 public class BigIntegerMarshaller {
 
-    static final BigInteger compareLimit = new BigInteger("0");
+    static final BigInteger minLimit = new BigInteger("0");
+    static final BigInteger maxLimit = new BigInteger("18446744073709551615");
     /*
      * convert a BigInteger to a String
      * The BigInteger must be greater or equal to 0
@@ -26,10 +27,10 @@ public class BigIntegerMarshaller {
      *
      */
     public static String serializeBigInteger(BigInteger bigInt) throws JiBXException {
-        if (bigInt.compareTo(compareLimit) >= 0) {
+        if ((bigInt.compareTo(minLimit) >= 0) && (bigInt.compareTo(maxLimit) <= 0)) {
             return bigInt.toString();
         } else {
-            throw new JiBXException("unsignedLong must be greater than or equal to 0");
+            throw new JiBXException("unsignedLong must be greater than or equal to 0 and less than or equal to 18446744073709551615");
         }
 
     }
@@ -48,10 +49,10 @@ public class BigIntegerMarshaller {
     	if (bigIntValue == null)
     		throw new JiBXException("serialized value cannot be null");
     	BigInteger bigInt = new BigInteger(bigIntValue);
-        if (bigInt.compareTo(compareLimit) >= 0) {
+        if ((bigInt.compareTo(minLimit) >= 0) && (bigInt.compareTo(maxLimit) <= 0)) {
             return bigInt;
         } else {
-            throw new JiBXException("unsignedLong must be greater than or equal to 0");
+            throw new JiBXException("unsignedLong must be greater than or equal to 0 and less than or equal to 18446744073709551615");
         }
     }
 }
---
+++ src/test/java/org/dataone/service/util/BigIntegerMashallerTestCase.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.dataone.service.util;
+
+import java.math.BigInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jibx.runtime.JiBXException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ *
+ * @author waltz
+ */
+public class BigIntegerMashallerTestCase {
+
+    @Test
+    public void deserializeBigIntegerTestSuccess() {
+        String someBigNumber = "12345123451234512345";
+        BigInteger someBigInteger = null;
+        try {
+            someBigInteger = BigIntegerMarshaller.deserializeBigInteger(someBigNumber);
+        } catch (JiBXException ex) {
+           ex.printStackTrace();
+           fail(ex.getMessage());
+        }
+        String anotherBigNumber = "0";
+        try {
+            anotherBigNumber = BigIntegerMarshaller.serializeBigInteger(someBigInteger);
+        } catch (JiBXException ex) {
+           ex.printStackTrace();
+           fail(ex.getMessage());
+        }
+        assertTrue(someBigNumber.equals(anotherBigNumber));
+    }
+    @Test
+    public void deserializeBigIntegerTestFailures() {
+        String someBigNumber = "512345123451234512345";
+
+        BigInteger someBigInteger = null;
+        boolean failed = false;
+        try {
+            someBigInteger = BigIntegerMarshaller.deserializeBigInteger(someBigNumber);
+        } catch (JiBXException ex) {
+           assertTrue(ex.getMessage().contains("unsignedLong must be greater"));
+           failed = true;
+        }
+        assertTrue(failed);
+        failed = false;
+        someBigInteger = BigInteger.valueOf(Long.MAX_VALUE);
+        someBigInteger = someBigInteger.multiply(someBigInteger);
+
+        try {
+            someBigNumber = BigIntegerMarshaller.serializeBigInteger(someBigInteger);
+        } catch (JiBXException ex) {
+           assertTrue(ex.getMessage().contains("unsignedLong must be greater"));
+           failed = true;
+        }
+
+        failed = false;
+        String someNegativeNumber = "-1";
+        try {
+            someBigInteger = BigIntegerMarshaller.deserializeBigInteger(someNegativeNumber);
+        } catch (JiBXException ex) {
+           assertTrue(ex.getMessage().contains("unsignedLong must be greater"));
+           failed = true;
+        }
+        assertTrue(failed);
+
+        failed = false;
+        someBigInteger = BigInteger.valueOf(-1L);
+        try {
+            someBigNumber = BigIntegerMarshaller.serializeBigInteger(someBigInteger);
+        } catch (JiBXException ex) {
+           assertTrue(ex.getMessage().contains("unsignedLong must be greater"));
+           failed = true;
+        }
+        assertTrue(failed);
+    }
+}
