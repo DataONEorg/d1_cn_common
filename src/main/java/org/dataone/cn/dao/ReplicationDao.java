40530c78343d8820bc71244e42f4651d5840cd07
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-04-23 20:10:23 +0000

    removing dependency to spring (PagedListHolder)

--- src/main/java/org/dataone/cn/dao/ReplicationDao.java
+++ src/main/java/org/dataone/cn/dao/ReplicationDao.java
@@ -20,9 +20,9 @@
 package org.dataone.cn.dao;
 
 import java.util.Date;
+import java.util.List;
 
 import org.dataone.service.types.v1.Identifier;
-import org.springframework.beans.support.PagedListHolder;
 
 /**
  * Abstract definition of a replication data access object used to identify
@@ -33,13 +33,12 @@ import org.springframework.beans.support.PagedListHolder;
  */
 public interface ReplicationDao {
 
-    public PagedListHolder<Identifier> getReplicasByDate(Date auditDate, int pageSize,
-            int pageNumber);
+    public List<Identifier> getReplicasByDate(Date auditDate, int pageSize, int pageNumber);
 
-    public PagedListHolder<Identifier> getFailedReplicas(int pageSize, int pageNumber);
+    public List<Identifier> getFailedReplicas(int pageSize, int pageNumber);
 
-    public PagedListHolder<Identifier> getInvalidReplicas(int pageSize, int pageNumber);
+    public List<Identifier> getInvalidReplicas(int pageSize, int pageNumber);
 
-    public PagedListHolder<Identifier> getStaleQueuedRelicas(int pageSize, int pageNumber);
+    public List<Identifier> getStaleQueuedRelicas(int pageSize, int pageNumber);
 
 }
