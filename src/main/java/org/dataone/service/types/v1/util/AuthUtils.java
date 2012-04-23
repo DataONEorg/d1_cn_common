8fc6453d3143a9b29fa0e20173ddc459b8539599
Rob Nahf <rnahf@epscor.unm.edu>
2012-04-23 22:40:12 +0000

    Refactored the AuthUtils authorization methods so that daisy-chained equivalent identities can be traversed (transitive group membership and transitive verification).  Created a slew of unit tests.

--- src/main/java/org/dataone/service/types/v1/util/AuthUtils.java
+++ src/main/java/org/dataone/service/types/v1/util/AuthUtils.java
@@ -1,6 +1,5 @@
 package org.dataone.service.types.v1.util;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -20,158 +19,204 @@ import org.dataone.service.util.Constants;
 public class AuthUtils {
 
 	private static Logger logger = Logger.getLogger(AuthUtils.class);
-
+	
+	private static Subject verifiedSubject = null;
+	
+	
 	/**
 	 * Derived from Metacat implementation
 	 * Creates a list of subjects represented in the session object, parsing 
 	 * both the subject of the session and the subjectInfo.
 	 * 
-	 * Does not handle administrative access / authorization
+	 * - It will always include 'public' subject
+	 * 
+	 * - Includes 'authenticated' subject when the session subject is not empty or 'public'
+	 * - Zeros-out the subjectInfo of the session if the subject of the session is
+	 *   null or 'public'
+	 * - 'verified' is included if primary subject or equivalent identity in the 
+	 *   subectInfo is verified.
+	 * - group membership is transitive, too - the group subjects of equivalent 
+	 *   identities are returned as well.
+	 * - Does not handle administrative access / authorization
+	 * 
 	 * @param session
-	 * @return
+	 * @return - a Subject array.
 	 */
 	public static Subject[] authorizedClientSubjects(Session session)
 	{
-		// for the "Verified" symbolic user
-		Subject verifiedSubject = new Subject();
-		verifiedSubject.setValue(Constants.SUBJECT_VERIFIED_USER);
-
+		// setup a static subject for verified symbolic user
+		if (verifiedSubject == null) {
+			verifiedSubject = new Subject();
+			verifiedSubject.setValue(Constants.SUBJECT_VERIFIED_USER);
+		}
+		
 		// get the subject[s] from the session
 		Set<Subject> subjects = new TreeSet<Subject>();
-		if (session != null) {
-			// primary subject
-			Subject subject = session.getSubject();
-			if (subject != null) {
-				subjects.add(subject);
-			}
-			// details about the subject
-			SubjectInfo subjectInfo = session.getSubjectInfo();
-			if (subjectInfo != null) {
-				// find subjectInfo for the primary subject
-				List<Person> personList = subjectInfo.getPersonList();
-				List<Group> groupList = subjectInfo.getGroupList();
-				if (personList != null) {
-					for (Person p : personList) {
-						// for every person listed (isVerified is transitive)
-						logger.debug("checking person");
-						logger.debug("p.getVerified(): " + p.getVerified());
-						if (p.getVerified() != null && p.getVerified()) {
-							// add the verified symbolic user
-							subjects.add(verifiedSubject);
-						}
-						// add the equivalent identities
-						List<Subject> equivList = p.getEquivalentIdentityList();
-						if (equivList != null) {
-							for (Subject equivSubject : equivList) {
-								subjects.add(equivSubject);
-								// find that entry
-								for (Person equivPerson: personList) {
-									if (equivSubject.equals(equivPerson.getSubject())) {
-										// transitive group membership
-										if (equivPerson.getIsMemberOfList() != null) {
-											for (Subject equivGroup: equivPerson.getIsMemberOfList()) {
-												subjects.add(equivGroup);
-											}
-										}
-										// TODO: is verified transitive?
-										if (equivPerson.getVerified() != null && equivPerson.getVerified()) {
-											// add the verified symbolic user
-											subjects.add(verifiedSubject);	
-										}
-									}
-								}
-							}
-						}
-						// add the groups they are a member of
-						List<Subject> memberOfList = p.getIsMemberOfList();
-						if (memberOfList != null) {
-							for (Subject g : memberOfList) {
-								subjects.add(g);
-							}
-						}
-						// look at all the Groups to see if this person has membership defined there
-						if (groupList != null) {
-							for (Group group: groupList) {
-								if (group.getHasMemberList() != null) {
-									for (Subject member: group.getHasMemberList()) {
-										// is the person a member?
-										if (member.equals(p.getSubject())) {
-											// add this group as a subject to check if it is not already there
-											subjects.add(group.getSubject());
-										}
-									}
-								}
-							}
-						}
-						break;
-					}
-				}
-			}
-
-			// add the authenticated symbolic since we have a session
-			Subject authenticatedSubject = new Subject();
-			authenticatedSubject.setValue(Constants.SUBJECT_AUTHENTICATED_USER);
-			subjects.add(authenticatedSubject);
-		}
 
 		// add public subject for everyone
 		Subject publicSubject = new Subject();
 		publicSubject.setValue(Constants.SUBJECT_PUBLIC);
 		subjects.add(publicSubject);
-
-		return (Subject[]) subjects.toArray();
+		
+		if (session != null) {
+			
+			// parse the session subject as the primary subject
+			Subject primarySubject = session.getSubject();
+			if (primarySubject != null) {
+				subjects.add(primarySubject);
+				
+				// depending on the primary subject, can add the authenticated symbolic user
+				if (! primarySubject.getValue().equals(Constants.SUBJECT_PUBLIC) ) {
+					Subject s = new Subject();
+					s.setValue(Constants.SUBJECT_AUTHENTICATED_USER);
+					subjects.add(s);
+					
+				} else {
+					// zero out the subjectInfo for non-authenticated sessions
+					session.setSubjectInfo(null);
+				}
+			}
+			
+			// traverse the subjectInfo for more subjects 
+			SubjectInfo subjectInfo = session.getSubjectInfo();
+			if (subjectInfo != null) {
+				// get subjects for the primary subject within the subjectInfo of the session
+				findPersonsSubjects(subjects, subjectInfo, primarySubject);
+			}
+		}
+		return subjects.toArray(new Subject[0]);
 	}
+	
+
+	
+	/*
+	 * A recursive method to traverse the equivalent-identity relationships
+	 * and to handle the transitive nature of group membership and verified status.
+	 */
+	private static void findPersonsSubjects(Set<Subject> foundSubjects, SubjectInfo subjectInfo, Subject targetSubject) {
+
+		// setting this up for subsequent searches in the loop
+		List<Group> groupList = null;
+		if (subjectInfo != null) {
+			groupList = subjectInfo.getGroupList();
+		}
+		
+		if (subjectInfo != null && subjectInfo.getPersonList() != null) {
+			for (Person p: subjectInfo.getPersonList()) {
+				if (p.getSubject().equals(targetSubject)) {
+
+					// add targetSubject
+					foundSubjects.add(targetSubject);
+					logger.debug("traversing person: " + targetSubject.getValue());
+					
+					
+					// check verification status of this identity
+					if (p.getVerified() != null && p.getVerified()) {
+						foundSubjects.add(verifiedSubject);
+					}
+
+					// add the groups of this identity are a member of
+					List<Subject> memberOfList = p.getIsMemberOfList();
+					if (memberOfList != null) {
+						for (Subject g : memberOfList) {
+							foundSubjects.add(g);
+						}
+					}
+					
+					
+					// just in case the groups have the person as a member
+					// and the person doesn't list the group in isMemberOf
+					if (groupList != null) {
+						for (Group group: groupList) {
+							if (group.getHasMemberList() != null) {
+								for (Subject member: group.getHasMemberList()) {
+									// is the person a member?
+									if (member.equals(p.getSubject())) {
+										// add this group as a subject to check if it is not already there
+										foundSubjects.add(group.getSubject());
+									}
+								}
+							}
+						}
+					}
+					
+					// finally add equivalent identities
+					// (this is where the recursion happens)
+					for (Subject eqId : p.getEquivalentIdentityList()) {
+						// recursive call for un-examined targetSubjects
+						if (! foundSubjects.contains(eqId)) {
+							findPersonsSubjects(foundSubjects, subjectInfo, eqId);
+						}
+					}
+					
+				}
+			}
+		}
+	}
+	
 	
 	
 	/**
 	 * Needs testing!!
 	 * Queries the systemMetadata to see if one of the given subjects 
-	 * is allowed the specified permission.
-	 * the given systemMetadata of the object
+	 * is allowed the specified permission against the given systemMetadata
+	 * 
 	 * @param subjects - the list of subject, assumed to represent the subjects
 	 *                   of a session
-	 * @param permission -  the permission that is requested authorization for 
+	 * @param requestedPerm -  the permission that is requested authorization for 
 	 * @param systemMetadata - the systemMetadata of the target object to test
 	 * @return - true if one of the subjects is authorized for the given permission
 	 * @throws NotAuthorized
 	 */
-	public static boolean isAuthorized(Subject[] subjects, Permission permission, 
-	     SystemMetadata systemMetadata) 
+	public static boolean isAuthorized(Subject[] subjects, Permission requestedPerm,
+			SystemMetadata systemMetadata) 
 	throws NotAuthorized
 	{
 		boolean allowed = false;
-		// do we own it?
-	    for (Subject s: subjects) {
-	      logger.debug("Comparing \t" + 
-	                       systemMetadata.getRightsHolder().getValue() +
-	                       " \tagainst \t" + s.getValue());
-	    	allowed = systemMetadata.getRightsHolder().equals(s);
-	    	if (allowed) {
-	    		return allowed;
-	    	}
-	    }    
-	    
+
+		// if rightsHolder is one of subjects, can return allowed (true)
+		// without further checks
+		for (Subject s: subjects) {
+			allowed = systemMetadata.getRightsHolder().equals(s);
+			
+			logger.debug(String.format(
+					"Comparing \t%s \tagainst \t%s", 
+					systemMetadata.getRightsHolder().getValue(),
+					s.getValue())
+					);
+			
+			if (allowed) {
+				return allowed;
+			}
+		}    
+
 	    // otherwise check the access rules
 	    try {
 		    List<AccessRule> allows = systemMetadata.getAccessPolicy().getAllowList();
-		    search: // label break
-		    for (AccessRule accessRule: allows) {
-		      for (Subject s: subjects) {
-		        logger.debug("Checking allow access rule for subject: " + s.getValue());
-		        if (accessRule.getSubjectList().contains(s)) {
-		        	logger.debug("Access rule contains subject: " + s.getValue());
-		        	for (Permission p: accessRule.getPermissionList()) {
-			        	logger.debug("Checking permission: " + p.xmlValue());
-		        		List<Permission> expandedPermissions = expandPermissions(p);
-		        		allowed = expandedPermissions.contains(permission);
-		        		if (allowed) {
-				        	logger.info("Permission granted: " + p.xmlValue() + " to " + s.getValue());
-		        			break search; //label break
-		        		}
-		        	}
-	        		
-		        }
-		      }
+		    if (allows != null) {
+		    	search:
+		    		for (AccessRule accessRule: allows) {
+		    			if (accessRule.sizePermissionList() > 0) {
+		    				for (Subject s: subjects) {
+		    					if (accessRule.getSubjectList().contains(s)) {
+		    						if(requestedPerm.equals(Permission.READ)) {
+		    							// any permission will do, so skip the test
+		    							allowed = true;
+		    							break search;
+		    						} else if (requestedPerm.equals(Permission.CHANGE_PERMISSION)) {
+		    							// has to be exact match
+		    							allowed = accessRule.getPermissionList().contains(Permission.CHANGE_PERMISSION);
+		    						} else {
+		    							allowed = accessRule.getPermissionList().contains(Permission.CHANGE_PERMISSION)
+		    									|| accessRule.getPermissionList().contains(Permission.WRITE);
+		    						}
+		    						if (allowed) {
+		    							break search;
+		    						}
+		    					}
+		    				}
+		    			}
+		    		}
 		    }
 	    } catch (Exception e) {
 	    	// catch all for errors - safe side should be to deny the access
@@ -182,34 +227,10 @@ public class AuthUtils {
 	    
 	    // throw or return?
 	    if (!allowed) {
-	      throw new NotAuthorized("1820", permission + " not allowed on " + 
+	      throw new NotAuthorized("1820", requestedPerm + " not allowed on " + 
 	    		  systemMetadata.getIdentifier().getValue());
 	    }
 	    
-	    return allowed;
-	    
+	    return allowed;	    
 	}
-	
-	 /**
-	   * Given a Permission, returns a list of all permissions that it encompasses
-	   * Permissions are hierarchical so that WRITE also allows READ.
-	   * @param permission
-	   * @return list of included Permissions for the given permission
-	   */
-	  protected static List<Permission> expandPermissions(Permission permission) {
-		  	List<Permission> expandedPermissions = new ArrayList<Permission>();
-		    if (permission.equals(Permission.READ)) {
-		    	expandedPermissions.add(Permission.READ);
-		    }
-		    if (permission.equals(Permission.WRITE)) {
-		    	expandedPermissions.add(Permission.READ);
-		    	expandedPermissions.add(Permission.WRITE);
-		    }
-		    if (permission.equals(Permission.CHANGE_PERMISSION)) {
-		    	expandedPermissions.add(Permission.READ);
-		    	expandedPermissions.add(Permission.WRITE);
-		    	expandedPermissions.add(Permission.CHANGE_PERMISSION);
-		    }
-		    return expandedPermissions;
-	  }
 }
--- src/test/java/org/dataone/service/types/v1/util/AuthUtilsTestCase.java
+++ src/test/java/org/dataone/service/types/v1/util/AuthUtilsTestCase.java
@@ -1,24 +1,392 @@
 package org.dataone.service.types.v1.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.dataone.service.types.v1.Group;
+import org.dataone.service.types.v1.Person;
+import org.dataone.service.types.v1.Session;
 import org.dataone.service.types.v1.Subject;
+import org.dataone.service.types.v1.SubjectInfo;
+import org.dataone.service.util.Constants;
+import org.junit.Before;
 import org.junit.Test;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
 
 public class AuthUtilsTestCase {
 
-	@Test
-	public void test() {
-//		fail("Not yet implemented");
-	}
+	private Session standardSession = null;
+	
+	private Subject publick = null; 
+	private Subject authenticated = null; 
+	private Subject verified = null; 
+	
 
-	private Subject buildSubject(String s) {
+	private Subject buildSubject(String cn) {
 		Subject subject = new Subject();
-		subject.setValue(s);
+		subject.setValue("CN=" + cn + ",DC=something,DC=org");
 		return subject;
 	}
 	
-	public void testAuthorizedClientSubject() {
-	 	
+	private Person buildTestPerson(String subjectString) {
+		Person p = new Person();
+		p.setSubject(buildSubject(subjectString));
+		return p;
 	}
+	
+	private Group buildTestGroup(String subjectString) {
+		Group g = new Group();
+		g.setSubject(buildSubject(subjectString));
+		return g;
+	}
+	
+	
+	@Before
+	public void setupSymbolicSubjects() {
+		publick = new Subject();
+		publick.setValue(Constants.SUBJECT_PUBLIC);
+		
+		authenticated = new Subject();
+		authenticated.setValue(Constants.SUBJECT_AUTHENTICATED_USER);
+		
+		verified = new Subject();
+		verified.setValue(Constants.SUBJECT_VERIFIED_USER);
+	}
+	
+	
+	@Before
+	public void createSession() {
+		if (standardSession == null) {
+			Session ses = new Session();
+			Subject subjectX = buildSubject("x");
+			ses.setSubject(subjectX);
+
+			SubjectInfo si = new SubjectInfo();
+			Person x = buildTestPerson("x");
+			Person y = buildTestPerson("y");
+			Person z = buildTestPerson("z"); 
+			z.setVerified(true);
+			x.addEquivalentIdentity(y.getSubject());
+			x.addEquivalentIdentity(z.getSubject());
+			si.addPerson(x);		
+			si.addPerson(y);		
+			si.addPerson(z);		
+
+			ses.setSubjectInfo(si);
+			standardSession = ses;
+		}
+	}
+	
+	
+	@Test
+	public void testAuthorizedClientSubjects_Public() {
+		
+		
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(standardSession);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+		
+		
+		assertTrue("public should always appear", subjectList.contains(publick));
+	}
+
+	
+	@Test
+	public void testAuthorizedClientSubjects_Authenticated() {
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(standardSession);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+		
+	
+		assertTrue("authenticated should be in the list", subjectList.contains(authenticated));
+	}
+
+	
+	@Test
+	public void testAuthorizedClientSubjects_Verified() {
+		
+		
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(standardSession);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("verified appears because an equiv identity is verified", subjectList.contains(verified));
+	}
+
+	@Test
+	public void testAuthorizedClientSubjects_EquivalentIDs() {
+		
+		
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(standardSession);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("subject list should contain x", subjectList.contains(buildSubject("x")));
+		assertTrue("subject list should contain y", subjectList.contains(buildSubject("y")));
+		assertTrue("subject list should contain z", subjectList.contains(buildSubject("z")));
+	
+	}
+	
+	
+	@Test
+	public void testAuthorizedClientSubjects_DaisyChainEquivalentIDs() {
+		
+		Session ses = new Session();
+		Subject subjectX = buildSubject("x");
+		ses.setSubject(subjectX);
+
+		SubjectInfo si = new SubjectInfo();
+		Person x = buildTestPerson("x");
+		Person y = buildTestPerson("y");
+		Person z = buildTestPerson("z"); 
+		z.setVerified(true);
+		x.addEquivalentIdentity(y.getSubject());
+		y.addEquivalentIdentity(z.getSubject());
+		si.addPerson(x);		
+		si.addPerson(y);		
+		si.addPerson(z);		
+
+		ses.setSubjectInfo(si);
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(standardSession);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("subject list contains x", subjectList.contains(buildSubject("x")));
+		assertTrue("subject list contains y", subjectList.contains(buildSubject("y")));
+		assertTrue("subject list contains z", subjectList.contains(buildSubject("z")));
+	
+	}
+	
+	@Test
+	public void testAuthorizedClientSubjects_DaisyChainVerified() {
+		
+		Session ses = new Session();
+		Subject subjectX = buildSubject("x");
+		ses.setSubject(subjectX);
+
+		SubjectInfo si = new SubjectInfo();
+		Person x = buildTestPerson("x");
+		Person y = buildTestPerson("y");
+		Person z = buildTestPerson("z"); 
+		z.setVerified(true);
+		x.addEquivalentIdentity(y.getSubject());
+		y.addEquivalentIdentity(z.getSubject());
+		si.addPerson(x);		
+		si.addPerson(y);		
+		si.addPerson(z);		
+
+		ses.setSubjectInfo(si);
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(ses);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("subject list contains x", subjectList.contains(verified));
+	}
+
+	
+	@Test
+	public void testAuthorizedClientSubjects_EmptySession_isPublic() {
+		
+		Session ses = new Session();
+		
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(ses);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("empty session should revert to public",
+				subjectList.contains(publick));
+		assertTrue("empty session should have only 1 subject (public)",
+				subjectList.size() == 1);
+	}
+	
+	@Test
+	public void testAuthorizedClientSubjects_Null_isPublic() {
+		
+		
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(null);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("null session should revert to public",
+				subjectList.contains(publick));
+		assertTrue("null session should have only 1 subject (public)",
+				subjectList.size() == 1);
+	}
+	
+	
+	
+	@Test
+	public void testAuthorizedClientSubjects_PublicNotAuthenticated() {
+		
+		Session ses = new Session();
+		ses.setSubject(publick);
+
+		SubjectInfo si = new SubjectInfo();
+		Person x = buildTestPerson("x");
+		Person y = buildTestPerson("y");
+		Person z = buildTestPerson("z"); 
+
+		ses.setSubjectInfo(si);
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(ses);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertFalse("public subject in session, should not have authenticated",
+				subjectList.contains(authenticated));
+	}
+	
+	
+	@Test
+	public void testAuthorizedClientSubjects_GroupTransitive() {
+		
+		Session ses = new Session();
+		ses.setSubject(buildSubject("x"));
+
+		SubjectInfo si = new SubjectInfo();
+		Person x = buildTestPerson("x");
+		x.addIsMemberOf(buildSubject("groupA"));
+		Person y = buildTestPerson("y");
+		x.addEquivalentIdentity(buildSubject("y"));
+		y.addEquivalentIdentity(buildSubject("x"));		
+		y.addIsMemberOf(buildSubject("groupB"));
+		
+		Person z = buildTestPerson("z");
+		x.addEquivalentIdentity(buildSubject("z"));
+		z.addEquivalentIdentity(buildSubject("x"));	
+		z.addIsMemberOf(buildSubject("groupC"));
+
+		si.addPerson(x);
+		si.addPerson(y); 
+		si.addPerson(z);
+		
+		si.addGroup(buildTestGroup("groupA"));
+		si.addGroup(buildTestGroup("groupB"));
+		si.addGroup(buildTestGroup("groupC"));
+		ses.setSubjectInfo(si);
+		
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(ses);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("subject list should contain groupA", subjectList.contains(buildSubject("groupA")));
+		assertTrue("subject list should contain groupB", subjectList.contains(buildSubject("groupB")));
+		assertTrue("subject list should contain groupC", subjectList.contains(buildSubject("groupC")));
+	}
+	
+	
+	
+	
+	
+	@Test
+	public void testAuthorizedClientSubjects_GroupTransitive_DaisyChainEquivID() {
+		
+		Session ses = new Session();
+		ses.setSubject(buildSubject("x"));
+
+		SubjectInfo si = new SubjectInfo();
+		Person x = buildTestPerson("x");
+		Group a = buildTestGroup("groupA");
+		a.addHasMember(x.getSubject());
+		x.addIsMemberOf(a.getSubject());
+		
+		Person y = buildTestPerson("y");
+		Group b = buildTestGroup("groupB");
+		b.addHasMember(y.getSubject());
+		y.addIsMemberOf(b.getSubject());
+		
+		
+		x.addEquivalentIdentity(buildSubject("y"));
+		y.addEquivalentIdentity(buildSubject("x"));		
+		
+		
+		Person z = buildTestPerson("z");
+		Group c = buildTestGroup("groupC");
+		c.addHasMember(z.getSubject());
+		z.addIsMemberOf(c.getSubject());
+		
+		
+		y.addEquivalentIdentity(buildSubject("z"));
+		z.addEquivalentIdentity(buildSubject("y"));	
+		
+
+		si.addPerson(x);
+		si.addPerson(y); 
+		si.addPerson(z);	
+		
+		si.addGroup(a);
+		si.addGroup(b);
+		si.addGroup(c);
+		
+		ses.setSubjectInfo(si);
+		
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(ses);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("subject list should contain groupA", subjectList.contains(buildSubject("groupA")));
+		assertTrue("subject list should contain groupB", subjectList.contains(buildSubject("groupB")));
+		assertTrue("subject list should contain groupC", subjectList.contains(buildSubject("groupC")));
+	}
+	
+	
+	/**
+	 * setting up a circular equivalent identity graph, to make sure we don't get stuck
+	 */
+	@Test
+	public void testAuthorizedClientSubjects_RecursionInfiniteLoopTest() {
+		
+		Session ses = new Session();
+		ses.setSubject(buildSubject("x"));
+
+		SubjectInfo si = new SubjectInfo();
+		Person x = buildTestPerson("x");
+		Group a = buildTestGroup("groupA");
+		a.addHasMember(x.getSubject());
+		x.addIsMemberOf(a.getSubject());
+		
+		Person y = buildTestPerson("y");
+		Group b = buildTestGroup("groupB");
+		b.addHasMember(y.getSubject());
+		y.addIsMemberOf(b.getSubject());
+		
+		
+		x.addEquivalentIdentity(buildSubject("y"));
+		y.addEquivalentIdentity(buildSubject("x"));		
+		
+		
+		Person z = buildTestPerson("z");
+		Group c = buildTestGroup("groupC");
+		c.addHasMember(z.getSubject());
+		z.addIsMemberOf(c.getSubject());
+		
+		
+		y.addEquivalentIdentity(buildSubject("z"));
+		z.addEquivalentIdentity(buildSubject("y"));	
+		
+		
+		Person w = buildTestPerson("w");
+		Group d = buildTestGroup("groupD");
+		d.addHasMember(w.getSubject());
+		w.addIsMemberOf(d.getSubject());
+		
+		
+		z.addEquivalentIdentity(buildSubject("w"));
+		w.addEquivalentIdentity(buildSubject("z"));	
+		
+		x.addEquivalentIdentity(buildSubject("w"));
+		w.addEquivalentIdentity(buildSubject("x"));	
+
+		si.addPerson(w);
+		si.addPerson(x);
+		si.addPerson(y); 
+		si.addPerson(z);	
+		
+		si.addGroup(a);
+		si.addGroup(b);
+		si.addGroup(c);
+		si.addGroup(d);
+		
+		ses.setSubjectInfo(si);
+		
+		Subject[] subjects = AuthUtils.authorizedClientSubjects(ses);
+		List<Subject> subjectList = (List<Subject>)Arrays.asList(subjects);
+	
+		assertTrue("subject list should contain groupA", subjectList.contains(buildSubject("groupA")));
+		assertTrue("subject list should contain groupB", subjectList.contains(buildSubject("groupB")));
+		assertTrue("subject list should contain groupC", subjectList.contains(buildSubject("groupC")));
+		assertTrue("subject list should contain groupC", subjectList.contains(buildSubject("groupD")));
+	}
+	
 }
