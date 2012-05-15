dbb3ea468b09ab1160292c22bd932b0ea48903ff
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-05-15 17:06:41 +0000

    ReplicationPrioritizationStrategy and intial test class

--- src/test/java/org/dataone/cn/dao/ReplicationDaoMetacatImplTestUtil.java
+++ src/test/java/org/dataone/cn/dao/ReplicationDaoMetacatImplTestUtil.java
@@ -1,11 +1,14 @@
 package org.dataone.cn.dao;
 
+import java.sql.Types;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ReplicationDaoMetacatImplTestUtil {
 
     public static void createTables(JdbcTemplate jdbc) {
-        jdbc.execute("CREATE TABLE IF NOT EXISTS systemmetadatareplicationstatus " + //
+        jdbc.execute("CREATE TABLE IF NOT EXISTS systemmetadatareplicationstatus "
+                + //
                 "(guid text, member_node varchar(250), " + //
                 "status varchar(250), " + //
                 "date_verified timestamp)");//
@@ -15,4 +18,14 @@ public class ReplicationDaoMetacatImplTestUtil {
         jdbc.execute("DROP TABLE IF EXISTS systemmetadatareplicationstatus;");
     }
 
+    public static void createReplicationStatusRecord(JdbcTemplate jdbc,
+            String guid, String memberNodeId, String status, String dateVerified) {
+
+        int[] types = new int[] { Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                Types.TIMESTAMP };
+        jdbc.update(
+                "INSERT INTO systemmetadatareplicationstatus VALUES (?,?,?,?)",
+                new Object[] { guid, memberNodeId, status, dateVerified },
+                types);
+    }
 }
