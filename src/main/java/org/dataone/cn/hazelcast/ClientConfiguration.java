c48c89e561150695624a505704a4a7521a067176
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-11-13 19:12:23 +0000

    fixed use of deprecated hazelcast method

--- src/main/java/org/dataone/cn/hazelcast/ClientConfiguration.java
+++ src/main/java/org/dataone/cn/hazelcast/ClientConfiguration.java
@@ -22,42 +22,50 @@
 
 package org.dataone.cn.hazelcast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import com.hazelcast.config.Config;
 import com.hazelcast.config.FileSystemXmlConfig;
 import com.hazelcast.config.GroupConfig;
-import java.io.File;
-import java.io.FileNotFoundException;
+
 /**
- *
- * Read in a hazelcast configuration file and return settings such that
- * a client will only connect to the localhost
- *
- * Default is to read the storage cluster from /etc/dataone/storage/hazelcast.xml
- * But it can be overridden for testing purposes.
- *
- * TODO: may need to be written to support spring configurations as well as the standard
- * configuration so as to allow client configuration of processing and storage clusters
+ * 
+ * Read in a hazelcast configuration file and return settings such that a client
+ * will only connect to the localhost
+ * 
+ * Default is to read the storage cluster from
+ * /etc/dataone/storage/hazelcast.xml But it can be overridden for testing
+ * purposes.
+ * 
+ * TODO: may need to be written to support spring configurations as well as the
+ * standard configuration so as to allow client configuration of processing and
+ * storage clusters
  * 
  * @author waltz
  */
 public class ClientConfiguration {
 
     private static String storageConfigurationLocation = "/etc/dataone/storage/hazelcast.xml";
-    
+
     private Config hazelcastConfig;
+
     public ClientConfiguration() throws FileNotFoundException {
-            File hazelcastConfigfile = new File(storageConfigurationLocation);
-            hazelcastConfig = new FileSystemXmlConfig(hazelcastConfigfile);
+        File hazelcastConfigfile = new File(storageConfigurationLocation);
+        hazelcastConfig = new FileSystemXmlConfig(hazelcastConfigfile);
     }
+
     public ClientConfiguration(String location) throws FileNotFoundException {
-            File hazelcastConfigfile = new File(location);
-            hazelcastConfig = new FileSystemXmlConfig(hazelcastConfigfile);
+        File hazelcastConfigfile = new File(location);
+        hazelcastConfig = new FileSystemXmlConfig(hazelcastConfigfile);
     }
+
     public ClientConfiguration(Config hazelcastConfig) throws FileNotFoundException {
-            this.hazelcastConfig = hazelcastConfig;
+        this.hazelcastConfig = hazelcastConfig;
     }
+
     public int getPort() {
-        return hazelcastConfig.getPort();
+        return hazelcastConfig.getNetworkConfig().getPort();
     }
 
     public String getLocalhost() {
@@ -75,7 +83,7 @@ public class ClientConfiguration {
     }
 
     public void setStorageConfigurationLocation(String location) throws FileNotFoundException {
-        this.storageConfigurationLocation = location;
+        storageConfigurationLocation = location;
         File hazelcastConfigfile = new File(storageConfigurationLocation);
         hazelcastConfig = new FileSystemXmlConfig(hazelcastConfigfile);
     }
