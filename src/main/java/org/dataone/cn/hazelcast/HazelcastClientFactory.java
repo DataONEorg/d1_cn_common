f609eac15637f1fe83a321a9caf603c18e350ca2
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-12-04 02:40:20 +0000

    small factory refactor/cleanup

--- src/main/java/org/dataone/cn/hazelcast/HazelcastClientFactory.java
+++ src/main/java/org/dataone/cn/hazelcast/HazelcastClientFactory.java
@@ -64,49 +64,52 @@ public class HazelcastClientFactory {
     };
 
     public static HazelcastClient getStorageClient() {
-        return getHazelcastClient(hzStorageConfigSettingsOverrideLocation,
-                defaultStorageConfigLocation, hzStorageClient);
+        if (hzStorageClient == null) {
+            hzStorageClient = getHazelcastClient(hzStorageConfigSettingsOverrideLocation,
+                    defaultStorageConfigLocation);
+        }
+        return hzProcessingClient;
     }
 
     public static HazelcastClient getProcessingClient() {
-        return getHazelcastClient(hzProcessingConfigSettingsOverrideLocation,
-                defaultProcessingConfigLocation, hzProcessingClient);
+        if (hzProcessingClient == null) {
+            hzProcessingClient = getHazelcastClient(hzProcessingConfigSettingsOverrideLocation,
+                    defaultProcessingConfigLocation);
+        }
+        return hzProcessingClient;
     }
 
     private static HazelcastClient getHazelcastClient(String configLocationSetting,
-            String defaultLocation, HazelcastClient client) {
-        if (client == null) {
-            ClientConfiguration clientConfiguration = null;
-            try {
-                if (configLocationSetting != null) {
-                    // for testing purposes
-                    if (configLocationSetting.startsWith("classpath:")) {
-                        String hzConfigLocationConfig = configLocationSetting.replace("classpath:",
-                                "");
-                        ClasspathXmlConfig config = new ClasspathXmlConfig(hzConfigLocationConfig);
-                        clientConfiguration = new ClientConfiguration(config);
-                    } else {
-                        // to override the default behavior
-                        clientConfiguration = new ClientConfiguration(configLocationSetting);
-                    }
+            String defaultLocation) {
+        ClientConfiguration clientConfiguration = null;
+        try {
+            if (configLocationSetting != null) {
+                // for testing purposes
+                if (configLocationSetting.startsWith("classpath:")) {
+                    String hzConfigLocationConfig = configLocationSetting.replace("classpath:", "");
+                    ClasspathXmlConfig config = new ClasspathXmlConfig(hzConfigLocationConfig);
+                    clientConfiguration = new ClientConfiguration(config);
                 } else {
-                    clientConfiguration = new ClientConfiguration(defaultLocation);
+                    // to override the default behavior
+                    clientConfiguration = new ClientConfiguration(configLocationSetting);
                 }
-            } catch (FileNotFoundException ex) {
-                throw new NullPointerException("FileNotFound so clientConfiguration is Null: "
-                        + ex.getMessage());
+            } else {
+                clientConfiguration = new ClientConfiguration(defaultLocation);
             }
-
-            logger.info("group " + clientConfiguration.getGroup() + " pwd "
-                    + clientConfiguration.getPassword() + " addresses "
-                    + clientConfiguration.getLocalhost());
-
-            ClientConfig cc = new ClientConfig();
-            cc.getGroupConfig().setName(clientConfiguration.getGroup());
-            cc.getGroupConfig().setPassword(clientConfiguration.getPassword());
-            cc.addAddress(clientConfiguration.getLocalhost());
-            client = HazelcastClient.newHazelcastClient(cc);
+        } catch (FileNotFoundException ex) {
+            throw new NullPointerException("FileNotFound so clientConfiguration is Null: "
+                    + ex.getMessage());
         }
+
+        logger.info("group " + clientConfiguration.getGroup() + " pwd "
+                + clientConfiguration.getPassword() + " addresses "
+                + clientConfiguration.getLocalhost());
+
+        ClientConfig cc = new ClientConfig();
+        cc.getGroupConfig().setName(clientConfiguration.getGroup());
+        cc.getGroupConfig().setPassword(clientConfiguration.getPassword());
+        cc.addAddress(clientConfiguration.getLocalhost());
+        HazelcastClient client = HazelcastClient.newHazelcastClient(cc);
         return client;
     }
 }
